81b4989a66875cb471f244335d30e01069e7bd3c
chengchao <1244053432@qq.com>
2016-10-22 15:20:08 +0800

    8888

--- MyMasonryDemo-master/MyMasonryDemo/AppDelegate.m
+++ MyMasonryDemo-master/MyMasonryDemo/AppDelegate.m
@@ -15,10 +15,22 @@
     MainViewController *mainVC = [[MainViewController alloc] init];
     self.window.rootViewController = mainVC;
     [self.window makeKeyAndV
-- (void)applicationWillResignActive:(UIApplication *)application {
+- (void)applicationWillResignActive:(UIAppl
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     ication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. fsdfsfdsfdsfdsfdfsfsdGames should use this method to pause the game.
 }
+     
+     
+     
+     
      
      
      
